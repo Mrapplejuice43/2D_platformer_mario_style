c095bd0ff9694fd467142b944e3b019931172485
Gwendal <gwendal.carrique@gmail.com>
2020-06-03 21:30:06 +0200

    Changed constants names for clarity
    Created a new overlay for the editor

--- Overlay.py
+++ Overlay.py
@@ -1,16 +1,17 @@
 from Component import *
 
 # Events ids used inside pygame events not to get limited
-PLAY_EVENT = 1
-OPTION_EVENT = 2
-BACK_EVENT = 3
-EDITOR_EVENT = 4
-MAIN_MENU_EVENT = 5
+PLAY_ID = 1
+OPTION_ID = 2
+BACK_ID = 3
+EDITOR_ID = 4
+MAIN_MENU_ID = 5
 SELECT_GROUND = 6
 SELECT_BLOCK = 7
 SELECT_PLAYER = 8
 RESET_WORLD = 9
-QUIT_EVENT = 10
+QUIT_ID = 10
+SAVE_ID = 11
 
 
 class Overlay:
@@ -33,6 +34,7 @@ class Overlay:
             if not isinstance(cp, Background) and cp.pos[0] < pos[0] < cp.pos[0] + cp.width and cp.pos[1] < pos[1] < \
                     cp.pos[1] + cp.height:
                 cp.throwEvent()
+                return True
 
     def resize(self, width, height):
         for cp in self.components:
@@ -56,10 +58,10 @@ class Menu(Overlay):
     def __init__(self, width, height):
         super().__init__(width, height)
         self.addComponent(
-            Button(self, 300, 100, r"assets\\testButton.png", PLAY_EVENT, [50, 25], posType='percentFromCenter'),
-            Button(self, 300, 100, r"assets\\testButton.png", EDITOR_EVENT, [50, 50], posType='percentFromCenter'),
-            Button(self, 300, 100, r"assets\\testButton.png", QUIT_EVENT, [50, 75], posType='percentFromCenter'),
-            Button(self, 50, 50, r"assets\\testButton.png", BACK_EVENT, [95, 5], posType='percentFromCenter')
+            Button(self, 300, 100, r"assets\\testButton.png", PLAY_ID, [50, 25], posType='percentFromCenter'),
+            Button(self, 300, 100, r"assets\\testButton.png", EDITOR_ID, [50, 50], posType='percentFromCenter'),
+            Button(self, 300, 100, r"assets\\testButton.png", QUIT_ID, [50, 75], posType='percentFromCenter'),
+            Button(self, 50, 50, r"assets\\testButton.png", BACK_ID, [95, 5], posType='percentFromCenter')
         )
         self.components[0].addChild(Text(parent=self.components[0], content="Play", centered=True))
         self.components[1].addChild(Text(parent=self.components[1], content="Editor", centered=True))
@@ -71,9 +73,9 @@ class PauseMenu(Overlay):
         super().__init__(width, height)
         self.addComponent(
             Background(self, width, height),
-            Button(self, 300, 100, r"assets\\testButton.png", PLAY_EVENT, [50, 25], posType='percentFromCenter'),
+            Button(self, 300, 100, r"assets\\testButton.png", PLAY_ID, [50, 25], posType='percentFromCenter'),
             Button(self, 300, 100, r"assets\\testButton.png", RESET_WORLD, [50, 50], posType='percentFromCenter'),
-            Button(self, 300, 100, r"assets\\testButton.png", MAIN_MENU_EVENT, [50, 75], posType='percentFromCenter')
+            Button(self, 300, 100, r"assets\\testButton.png", MAIN_MENU_ID, [50, 75], posType='percentFromCenter')
 
         )
 
@@ -86,11 +88,26 @@ class EditorMenu(Overlay):
     def __init__(self, width, height):
         super().__init__(width, height)
         self.addComponent(
-            Button(self, 50, 50, r"assets\testButton.png", SELECT_GROUND, [10, 90], posType='percentFromCenter'),
-            Button(self, 50, 50, r"assets\testButton.png", SELECT_BLOCK, [20, 90], posType='percentFromCenter'),
-            Button(self, 50, 50, r"assets\testButton.png", SELECT_PLAYER, [30, 90], posType='percentFromCenter')
+            Button(self, 50, 50, r"assets\testButton.png", SELECT_GROUND, [10, 90], posType='percentFromCenter', eventType=EDITOR_EVENT),
+            Button(self, 50, 50, r"assets\testButton.png", SELECT_BLOCK, [20, 90], posType='percentFromCenter', eventType=EDITOR_EVENT),
+            Button(self, 50, 50, r"assets\testButton.png", SELECT_PLAYER, [30, 90], posType='percentFromCenter', eventType=EDITOR_EVENT)
         )
 
         self.components[0].addChild(Image(self.components[0], 50, 50, [0, 0], r"assets\topGround.png"))
         self.components[1].addChild(Image(self.components[1], 50, 50, [0, 0], r"assets\block.png"))
         self.components[2].addChild(Image(self.components[2], 50, 50, [0, 0], r"assets\balkany.png"))
+
+class EditorPauseMenu(Overlay):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        self.addComponent(
+            Background(self, width, height),
+            Button(self, 300, 100, r"assets\\testButton.png", PLAY_ID, [50, 25], posType='percentFromCenter'),
+            Button(self, 300, 100, r"assets\\testButton.png", SAVE_ID, [50, 50], posType='percentFromCenter'),
+            Button(self, 300, 100, r"assets\\testButton.png", MAIN_MENU_ID, [50, 75], posType='percentFromCenter')
+
+        )
+
+        self.components[1].addChild(Text(parent=self.components[1], content="Continue", centered=True))
+        self.components[2].addChild(Text(parent=self.components[2], content="Save World", centered=True))
+        self.components[3].addChild(Text(parent=self.components[3], content="Return to Menu", centered=True))
